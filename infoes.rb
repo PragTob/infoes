fb9782ffc3214269d380f671cd1d7f7839101efb
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-08 12:46:10 +0200

    We got gradiens, and we got real links

--- infoes.rb
+++ infoes.rb
@@ -1,16 +1,22 @@
 # infoes is a reader that aims to combine multiple services
 
+# yeah we actually need Twitter
+Shoes.setup do
+  gem 'twitter'
+end
+
+
 require './rss'
 
 Shoes.app :title => "infoes" do
-  background green
+  background gradient(lime, limegreen)
   stroke black
   title "This is infoes!", :align => "center"
   rss = load_rss "http://pragtob.wordpress.com/feed/"
   stack do
     rss.items.each do |each|
       para each.title
-      para each.link
+      para link("Go to Post", click: each.link)
     end
   end
 end
