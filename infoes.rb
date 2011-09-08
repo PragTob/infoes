2c3bde685f3356a67e243a72cf7073ab7de842e7
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-08 15:37:45 +0200

    Wow we now got a Twitter account setup page

--- infoes.rb
+++ infoes.rb
@@ -19,15 +19,27 @@ Shoes.app :title => "infoes" do
   flow do
     # menu items
     stack width: MENU_WIDTH do
-      para "Twitter account setup"
+      button "Twitter Settings" do
+        show_twitter_settings
+      end
     end
 
     rss = load_rss "http://pragtob.wordpress.com/feed/"
-    stack width: -MENU_WIDTH do
+    @content_box = stack width: -MENU_WIDTH do
       rss.items.each do |rss_item|
         para rss_item.title, " ", link("Go to Post", click: rss_item.link)
       end
     end
+  end
+end
+
+def show_twitter_settings
+  Shoes.app :title => "Twitter Settings" do
+    background gradient(deepskyblue, dodgerblue)
+    button "Connect infoes with twitter" do
+      alert "Implement me!!!!"
+    end
+    para link("Signe up at Twitter", click: "https://twitter.com/signup")
   end
 end
 
