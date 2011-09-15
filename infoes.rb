287e202c9647f7968b880716b5a17746dc6f5cde
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-15 21:36:49 +0200

    Hopefully functional ordering of methods for not my red shoes

--- infoes.rb
+++ infoes.rb
@@ -16,13 +16,103 @@ require 'launchy'
 require './lib/rss'
 require './lib/twitter'
 
-
 MENU_WIDTH = 120
 TWEET_PIC_WIDTH = 50
 
 # will become a preference
 TWEETS_TO_LOAD = 10
 TWITTER_SIGNUP = "https://twitter.com/signup"
+
+def done_button
+  button("Done") { close }
+end
+
+def restart_notice
+  para "Please note that you have to restart infoes in order for your changes ",
+    "to take effect. This is a very early alpha version. I am sorry."
+end
+
+# display the slot where a user may add a new url to his rss feeds
+def new_rss_feed_slot
+  flow do
+    @new_url_edit = edit_line width: 300
+    button("Add") do
+      new_url = @new_url_edit.text
+      RSSFeeds.add new_url
+
+      # add the newly entered URL to the list of urls
+      @main_stack.before(@new_url_slot) do
+        rss_feed_source new_url
+      end
+      # clear the edit_line so the user can enter a new feed url
+      @new_url_edit.text = ""
+    end
+  end
+end
+
+# creates a new RSS feed source entry for a given url
+def rss_feed_source(url)
+  this_entry = flow do
+    para url, " "
+    button("Remove") do
+      RSSFeeds.remove url
+      this_entry.clear
+    end
+  end
+end
+
+# Show the settings window for RSS
+def show_rss_settings
+  Shoes.app title: "RSS Feed Settings", width: 500, height: 300 do
+    background gradient(gold, darkorange)
+
+    @main_stack = stack do
+      restart_notice
+      RSSFeeds.urls.each do |url|
+        rss_feed_source url
+      end
+
+      @new_url_slot = new_rss_feed_slot
+      done_button
+    end
+  end
+end
+
+# the button that starts the connect to Twitter process
+def connect_to_twitter_button
+  button "Connect infoes with twitter" do
+    authorization_url = TwitterConnection.get_request_token
+    Launchy.open authorization_url
+    pincode = ask <<-eos
+                  A page should have been opened in your web browser.
+                  Please authorize this app and then enter the
+                  pincode displayed to you here.
+                eos
+
+    TwitterConnection.complete_authentication pincode
+    alert "Succesfully registered with Twitter!"
+    close
+  end
+end
+
+# show the seperate Twitter settings window
+def show_twitter_settings
+  Shoes.app :title => "Twitter Settings", :height => 300, :width => 300 do
+    background gradient(deepskyblue, royalblue)
+
+    stack do
+      unless TwitterConnection.already_authenticated?
+        restart_notice
+        connect_to_twitter_button
+        para link("Sign up at Twitter") { Launchy.open TWITTER_SIGNUP }
+      else
+        para "Your Twitter account is already connected to infoes!"
+      end
+      done_button
+    end
+  end
+end
+
 
 # the main menu displayed on the left hand side
 def menu
@@ -79,96 +169,6 @@ Shoes.app :title => "infoes" do
   flow do
     menu
     content
-  end
-end
-
-def done_button
-  button("Done") { close }
-end
-
-def restart_notice
-  para "Please note that you have to restart infoes in order for your changes ",
-    "to take effect. This is a very early alpha version. I am sorry."
-end
-
-# the button that starts the connect to Twitter process
-def connect_to_twitter_button
-  button "Connect infoes with twitter" do
-    authorization_url = TwitterConnection.get_request_token
-    Launchy.open authorization_url
-    pincode = ask <<-eos
-                  A page should have been opened in your web browser.
-                  Please authorize this app and then enter the
-                  pincode displayed to you here.
-                eos
-
-    TwitterConnection.complete_authentication pincode
-    alert "Succesfully registered with Twitter!"
-    close
-  end
-end
-
-# show the seperate Twitter settings window
-def show_twitter_settings
-  Shoes.app :title => "Twitter Settings", :height => 300, :width => 300 do
-    background gradient(deepskyblue, royalblue)
-
-    stack do
-      unless TwitterConnection.already_authenticated?
-        restart_notice
-        connect_to_twitter_button
-        para link("Sign up at Twitter") { Launchy.open TWITTER_SIGNUP }
-      else
-        para "Your Twitter account is already connected to infoes!"
-      end
-      done_button
-    end
-  end
-end
-
-# display the slot where a user may add a new url to his rss feeds
-def new_rss_feed_slot
-  flow do
-    @new_url_edit = edit_line width: 300
-    button("Add") do
-      new_url = @new_url_edit.text
-      RSSFeeds.add new_url
-
-      # add the newly entered URL to the list of urls
-      @main_stack.before(@new_url_slot) do
-        rss_feed_source new_url
-      end
-      # clear the edit_line so the user can enter a new feed url
-      @new_url_edit.text = ""
-    end
-  end
-end
-
-# creates a new RSS feed source entry for a given url
-def rss_feed_source(url)
-  this_entry = flow do
-    para url, " "
-    button("Remove") do
-      RSSFeeds.remove url
-      this_entry.clear
-    end
-  end
-end
-
-# Show the settings window for RSS
-def show_rss_settings
-  Shoes.app title: "RSS Feed Settings", width: 500, height: 300 do
-    background gradient(gold, darkorange)
-
-    @main_stack = stack do
-      restart_notice
-      RSSFeeds.urls.each do |url|
-        rss_feed_source url
-      end
-
-      @new_url_slot = new_rss_feed_slot
-      done_button
-    end
   end
 end
 
