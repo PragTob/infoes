33856ce6d31c3391debb25b44200c1629b62a95d
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-22 11:41:14 +0200

    refresh utils

--- tabs/home.rb
+++ tabs/home.rb
@@ -8,10 +8,23 @@ require './lib/settings'
 class Home < SideTab
 
   def content
+    refresh_utils
     items = []
     items.concat(RSSFeeds.entries).concat(TwitterConnection.tweets)
-    every(Settings.reload_interval) { clear { content } }
     items.sort.reverse.each { |each| each.display(self) }
+  end
+
+  private
+
+  def refresh_utils
+    every(Settings.reload_interval) { clear { content } }
+    flow do
+      button "Refresh" do
+        clear { content }
+      end
+      inscription "(This will automatically happen every " +
+                  "#{Settings.reload_interval / 60} minutes)"
+    end
   end
 
 end
